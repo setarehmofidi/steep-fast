f40f60e16ba316b8a56f1cb5f02c0c5dfd878a9e
setarehmofidi <setareh.seyedmofidi@pcc.edu>
2020-01-09 15:47:09 -0800

    baby green

--- sketch.js
+++ sketch.js
@@ -5,7 +5,7 @@ function setup() {
 
 function draw() {
   // put drawing code here
-  background(200,200,255);
+  background(200,255,200);
   fill(0, 10);
   for(let i=0; i<50; i++){
   triangle(mouseX*i,mouseY+i,mouseX,i+200,i,i);
