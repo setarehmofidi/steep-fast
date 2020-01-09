f5b8181e7c5b67238dd984ef3ccb1c4f5a9198f1
setarehmofidi <setareh.seyedmofidi@pcc.edu>
2020-01-09 11:36:05 -0800

    changed background

--- sketch.js
+++ sketch.js
@@ -5,9 +5,9 @@ function setup() {
 
 function draw() {
   // put drawing code here
-  background(255,200,255);
+  background(200,200,255);
   fill(0, 10);
-  for(let i=0; i<100; i++){
+  for(let i=0; i<50; i++){
   triangle(mouseX*i,mouseY+i,mouseX,i+200,i,i);
 }
 }
