842f6dfe0b1b67396b9a23702b184f8d6df74a99
setarehmofidi <setareh.seyedmofidi@pcc.edu>
2020-01-21 12:56:54 -0800

    fast movement

--- sketch.js
+++ sketch.js
@@ -7,10 +7,16 @@ function draw() {
   // put drawing code here
   let red= map(mouseX,0,width, 180,255)
   let blue = map(mouseY,0,height, 180,255)
+  let x=displayWidth
 
   background(red,210,blue);
+  strokeWeight(0.8)
   fill(0, 10);
+  //for(let move=0; move<50; move++){
   for(let i=0; i<50; i++){
-  triangle(50*i,i-20,1000,i+400,i,i*i);
-}
-}
+  	x=x-0.05
+  mouseX = map(mouseX,0,displayWidth,x,0)	
+  triangle(50*i,i-20,mouseX*1.65,i+410,i,i*i);
+	}
+  
+}  
