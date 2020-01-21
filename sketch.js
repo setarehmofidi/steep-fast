4c62591cbf309e0ff26741a2dc8d378bdb4f17d4
setarehmofidi <setareh.seyedmofidi@pcc.edu>
2020-01-21 11:04:21 -0800

    change background

--- sketch.js
+++ sketch.js
@@ -5,7 +5,10 @@ function setup() {
 
 function draw() {
   // put drawing code here
-  background(255,210,255);
+  let red= map(mouseX,0,width, 180,255)
+  let blue = map(mouseY,0,height, 180,255)
+
+  background(red,210,blue);
   fill(0, 10);
   for(let i=0; i<50; i++){
   triangle(50*i,i-20,1000,i+400,i,i*i);
